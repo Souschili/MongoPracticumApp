db87e9d260c97b09a36c7fa9d3206f11d5349270
Orkhan <aliyevorhan82@gmail.com>
2024-10-12 04:43:35 +0400

    regenerate sample data and update local collection create 2 task

--- MongoEFCoreApp/Program.cs
+++ MongoEFCoreApp/Program.cs
@@ -7,22 +7,36 @@ namespace MongoEFCoreApp
 {
     internal class Program
     {
-        private static IMongoDatabase _dataBase=new MongoClient().GetDatabase("Demo");
-        private static IMongoCollection<User> _usersCol=_dataBase.GetCollection<User>("users");
-     
+        private static IMongoDatabase _dataBase = new MongoClient().GetDatabase("Demo");
+        private static IMongoCollection<User> _usersCol = _dataBase.GetCollection<User>("users");
+
         static async Task Main(string[] args)
         {
             // seed users to mongo
             // генератор записей для коллекции 
             //await MongoSeedData.UploadUserData(_usersCol);
+            await ReadContern();
         }
 
         // тут запросы на чтение в двух стилях лямбды и фильтры
-        public static void ReadConcern()
+        public static async Task ReadContern()
         {
-            
+            // бьюлдер для фильтров ,добавлен для фильрационых запросов
+            FilterDefinitionBuilder<User> filterBuilder = new FilterDefinitionBuilder<User>();
 
+            //1.Найдите одного пользователя с именем "Leyla".
+            //FilterDefinition<User> filterTask1= Builders<User>.Filter.Eq(x=> x.Name,"Leyla");
+            var task1 = await _usersCol.Find(x => x.Name == "Leyla")
+                .FirstOrDefaultAsync();
+            Console.WriteLine(task1.ToJson());
+
+            // 2. Получите всех пользователей старше 30 лет.
+            //var filterTask2 = filterBuilder.Gt(x => x.Age, 30);
+            var task2 = await _usersCol.Find(x => x.Age > 30).ToListAsync();
+            // вывод данных
+            //Console.WriteLine($"Найдено {task2.Count} записей");
+            //task2.ForEach(x => Console.WriteLine(x.ToJson()));
         }
-        
+
     }
 }
